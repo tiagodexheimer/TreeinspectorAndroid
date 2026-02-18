38a0ce1939f2f23ef082713af3ef66fcd5b77fd6
Tiago Dexheimer <tiagodexheimer@gmail.com>
2026-02-18 09:32:21 -0300

    Inicial version v1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 		applicationId = "com.dexheimer.treeinspectorandroid"
 		minSdk = 24
 		targetSdk = 36
-		versionCode = 11
-		versionName = "1.1"
+		versionCode = 12
+		versionName = "1.2"
 
 		testInstrumentationRunner = "com.dexheimer.treeinspectorandroid.CustomTestRunner"
 	}
